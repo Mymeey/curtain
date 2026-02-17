d457e50f70c7e622d08f5578a7f0ec43578ec0b3
Mymee <daikikozawa02100@gmail.com>
2026-02-17 19:58:21 +0900

    Downgrade to Next.js 14 to fix hydration errors

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
